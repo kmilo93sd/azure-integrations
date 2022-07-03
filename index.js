dfa20114526e7a476afc80c2ba225e0fbde792b7
Camilo Alaniz <kmilo93sd@gmail.com>
2022-07-02 22:55:17 -0400

    now organization, username and token are given in the request. Added invalid token error handling

--- index.js
+++ index.js
@@ -5,20 +5,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const getToken = () => {
-  const token = process.env.AUTH_TOKEN || null;
-
-  if(!token) {
-    throw new Error('Missing token');
-  }
-  return token;
-}
-
-const generateAuth = (username, token) => {
+const generateToken = (username, token) => {
   return Buffer.from(`${username}:${token}`).toString('base64');
 }
-
-const AUTH_TOKEN = getToken();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -26,14 +15,28 @@ app.get('/', (req, res) => {
 
 app.get('/projects', async (request, response) => {
     try {
-      const path = 'https://dev.azure.com/kmilo93sd/_apis/projects?api-version=6.0';
-      const auth_token = generateAuth('kmilo93sd', AUTH_TOKEN);
-      console.log('auth token:', auth_token)    
-      const result = await axios.get(path,{ headers: { Authorization: `Basic ${auth_token}` } });
-      console.log('PROJECTS', result.data);
+      
+      const { token } = request.headers;
+      if(!token) {
+        return response.status(400).json({ message: 'Missing token' });
+      }
+      const { organization, username } = request.query;
+      if(!organization) {
+        return response.status(400).json({ message: 'Missing organization' });
+      }
+      if(!username) {
+        return response.status(400).json({ message: 'Missing username' });
+      }
+      
+      const path = `https://dev.azure.com/${organization}/_apis/projects?api-version=6.0`;
+      const authToken = generateToken(username, token);  
+      const result = await axios.get(path,{ headers: { Authorization: `Basic ${authToken}` } });
       return response.json({ projects: result.data });
     } catch (error) {
       console.log("ERROR:", error.message);
+      if(error.response && error.response.status === 401) {
+        return response.status(401).json({ message: "Provided token is invalid" }); 
+      }
       return response.status(500).json({ message: "Something was wrong on call api service" });      
     }
 });
